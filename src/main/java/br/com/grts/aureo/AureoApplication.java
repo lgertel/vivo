1e8ac380179eafdf5dd22d3d24f22327e62afcb8
Lucas Gertel <lgertel@gmail.com>
2019-01-10 10:42:02 -0200

    First Commit # Aureo Microservice using Spring boot.

---
+++ src/main/java/br/com/grts/aureo/AureoApplication.java
@@ -0,0 +1,14 @@
+package br.com.grts.aureo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AureoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AureoApplication.class, args);
+  }
+
+}
+
---
+++ src/test/java/br/com/grts/aureo/AureoApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.grts.aureo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AureoApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
